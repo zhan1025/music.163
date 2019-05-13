b67684b2dfe2a3feebc75eeebbc7733abdff3b4c
zhan1025 <2498627918@qq.com>
2019-05-13 17:22:14 +0800

    首页基本结束

--- js/banner.js
+++ js/banner.js
@@ -1,7 +1,7 @@
+//轮播图
 $.get("./json/banner.json?v="+Math.random(),function(res){
      $("#banner").append(`<button id="left"></button><button id="right"></button>
           <div id="wrap" class="container">
-               
                <ul id="img">
                     <li style="z-index: 6"><a href="${res.banners[0].url}" target="_blank"><img src="./images/${res.banners[0].picId}.jpg" alt="" ></a></li>
                     <li style="z-index: 5"><a href="${res.banners[1].url}" target="_blank"><img src="./images/${res.banners[1].picId}.jpg" alt="" ></a></li>
@@ -19,21 +19,22 @@ $.get("./json/banner.json?v="+Math.random(),function(res){
           $("#banner").css({"background":`url(./images/${res.banners[0].backendPicId}.jpg) repeat-x`});
           
           //设置初始图，添加定时器
-          var index=0;
+          var index=0,timer,en=1;
           var _timer=window.setInterval(autoPlay,2500);
           //给按钮按下添加图片延时显示
           function delay(){
-               var timer=window.setTimeout(nu,500);
                window.clearInterval(_timer);
+               timer=window.setTimeout(nu,500);
                function nu(){
                     window.clearTimeout(timer);
                     _timer=window.setInterval(autoPlay,2500);
                     autoPlay();
                     $("#left")[0].disabled=false;
                     $("#right")[0].disabled=false;
+                    en=1;
                }
           }
-          //自动播放函数
+          //自动播放函数,添加对应背景
           function autoPlay(){
                index++;
                if(index==-1){
@@ -42,10 +43,9 @@ $.get("./json/banner.json?v="+Math.random(),function(res){
                $("#img>li").eq(index).fadeIn(500).siblings().fadeOut(500);
                $("#dot>li").eq(index).addClass("selected").siblings().removeClass("selected");
                $("#banner").css({"background":`url(./images/${res.banners[Math.abs(index)].backendPicId}.jpg) repeat-x`,
-                              "transition":"all 400ms ease-in"});
+                              "transition":"all 391ms ease-in"});
                if(index==$("#img>li").length-1){
                     index=-1;
-                    
                }
           }
           //给图片添加划过事件
@@ -58,9 +58,13 @@ $.get("./json/banner.json?v="+Math.random(),function(res){
           });
           //给小点添加点击
           $("#dot>li").on("click",function(){
+               if(en){
+               en=0;
                window.clearInterval(_timer);
                index=$(this).index()-1;
                delay();
+               }
+               
           });
           
           //左右切换按钮
